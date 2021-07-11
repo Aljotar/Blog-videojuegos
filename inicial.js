8d64ed776734b9130de23dd7dc44a7b7c978a443
Jotar Alvaro <jotaralvaro@gmail.com>
2021-07-11 00:55:27 -0300

    animacion js logo Ps5

--- inicial.js
+++ inicial.js
@@ -1,10 +1,16 @@
 window.addEventListener('scroll',function(){
     let animacion = document.getElementById('carousel');
+    let animacionPs5 = document.getElementById('img_ps5');
     let posicionCarousel = animacion.getBoundingClientRect().top;
-    console.log(posicionCarousel);
+    let posicionPs5 = animacionPs5.getBoundingClientRect().top;
+    console.log(posicionPs5);
     let tamañoPantalla = window.innerHeight;
 
     if(posicionCarousel < 900){
         animacion.style.animation = 'carousel 1s ease-out'
     }
+    if(posicionPs5 < 720){
+        animacionPs5.style.animation = 'animacion_ps5 2s ease-out'
+    }
 })
+
